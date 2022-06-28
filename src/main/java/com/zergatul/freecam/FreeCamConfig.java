412f19244b2dace8b8957cd9e2070974ff4fee85
Igor Budzhak <z3rgatul@gmail.com>
2022-06-28 18:28:45 +0300

    added config

--- src/main/java/com/zergatul/freecam/FreeCamConfig.java
+++ src/main/java/com/zergatul/freecam/FreeCamConfig.java
@@ -1,13 +1,36 @@
 package com.zergatul.freecam;
 
 public class FreeCamConfig {
+
+    public static final double MinAcceleration = 5;
+    public static final double DefaultAcceleration = 50;
+    public static final double MaxAcceleration = 500;
+    public static final double MinMaxSpeed = 5;
+    public static final double DefaultMaxSpeed = 50;
+    public static final double MaxMaxSpeed = 500;
+    public static final double MinSlowdownFactor = 1e-9;
+    public static final double DefaultSlowdownFactor = 0.01;
+    public static final double MaxSlowdownFactor = 0.5;
+
     public double acceleration;
     public double maxSpeed;
     public double slowdownFactor;
 
     public FreeCamConfig() {
-        acceleration = 1;
-        maxSpeed = 50;
-        slowdownFactor = 0.01;
+        acceleration = DefaultAcceleration;
+        maxSpeed = DefaultMaxSpeed;
+        slowdownFactor = DefaultSlowdownFactor;
+    }
+
+    public void clamp() {
+        if (acceleration < MinAcceleration || acceleration > MaxAcceleration) {
+            acceleration = DefaultAcceleration;
+        }
+        if (maxSpeed < MinMaxSpeed || maxSpeed > MaxMaxSpeed) {
+            maxSpeed = DefaultMaxSpeed;
+        }
+        if (slowdownFactor < MinSlowdownFactor || slowdownFactor > MaxSlowdownFactor) {
+            slowdownFactor = DefaultSlowdownFactor;
+        }
     }
 }
--- src/main/java/com/zergatul/freecam/KeyBindingsController.java
+++ src/main/java/com/zergatul/freecam/KeyBindingsController.java
@@ -10,11 +10,8 @@ import org.lwjgl.glfw.GLFW;
 public class KeyBindingsController {
 
     public static final KeyBindingsController instance = new KeyBindingsController();
-    public static final String category = "FreeCam by Zergatul";
 
-    public static KeyMapping toggleFreeCam = new KeyMapping("Toggle free cam", GLFW.GLFW_KEY_F6, category);
-
-    private Minecraft mc = Minecraft.getInstance();
+    public static final KeyMapping toggleFreeCam = new KeyMapping("key.zergatul.freecam.toggle", GLFW.GLFW_KEY_F6, "category.zergatul.freecam");
 
     private KeyBindingsController() {
 
@@ -22,20 +19,5 @@ public class KeyBindingsController {
 
     public void setup() {
         ClientRegistry.registerKeyBinding(toggleFreeCam);
-    }
-
-    @SubscribeEvent
-    public void onKeyInputEvent(InputEvent.KeyInputEvent event) {
-
-        if (mc.player == null) {
-            return;
-        }
-        if (mc.screen != null) {
-            return;
-        }
-
-        if (KeyBindingsController.toggleFreeCam.isDown()) {
-            FreeCamController.instance.toggle();
-        }
     }
 }
